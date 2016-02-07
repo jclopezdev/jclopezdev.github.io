686d9f22ca09a7204dfc2cb297559ecf9edc3a65
Juan Carlos López <j.carloslopezl@hotmail.com>
2016-02-07 04:41:46 -0600

    Add sass instead of stylus

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,23 @@
 var gulp = require('gulp');
 var webserver = require("gulp-webserver");
+var sass = require('gulp-sass');
+
+// Config vars
+var config = {
+  styles: {
+    main: 'static/sass/styles.scss',
+    watch: 'static/sass/**/*.scss',
+    output: 'static/css'
+  },
+  html: {
+    watch: './*.html'
+  },
+  scripts: {
+    main: 'static/js/app.js',
+    watch: 'static/**/*.js',
+    output: 'static/js'
+  }
+};
 
 // Run local webserver
 gulp.task('server', function(){
@@ -11,5 +29,21 @@ gulp.task('server', function(){
 		}));
 });
 
+// Build css
+gulp.task('build:css', function () {
+ gulp.src(config.styles.watch)
+   .pipe(sass({outputStyle: 'expanded'})) // compressed, expanded
+   .pipe(gulp.dest(config.styles.output));
+});
+
+// Watch task
+gulp.task('watch', function() {
+  gulp.watch(config.styles.watch,   ['build:css']);    
+  gulp.watch(config.html.watch,     ['build']);
+});
+
+// Build Task
+gulp.task('build', ['build:css']);
+
 // Default gulp task
-gulp.task('default', ['server']);
+gulp.task('default', ['server', 'watch', 'build']);
